e000ee3e45a78645dbec27cdfe458b72cb5dafa3
jaylions <jaylions@snu.ac.kr>
2022-09-09 15:15:40 +0900

    solution input output done

--- seminar0/src/main/kotlin/com/wafflestudio/seminar/test/Solution.kt
+++ seminar0/src/main/kotlin/com/wafflestudio/seminar/test/Solution.kt
@@ -7,4 +7,47 @@ package com.wafflestudio.seminar.test
  */
 fun main() {
     // 여기를 채워 주세요!
+    var firstInput=readLine();
+    if (firstInput != null) {
+        val sliceNum=firstInput.length-2
+        firstInput=firstInput.slice(IntRange(2,sliceNum))
+    }
+    println(firstInput)
+    var mainArray= firstInput?.split("\",\"")
+    println(mainArray)
+    var currentSelect=0;
+    while (true){
+        var command= readLine().toString();
+        var commandArray=command.split(" ")
+        if(commandArray[0]=="move"){
+            /*
+            move -u <숫자>
+            현재 선택된 행에서 입력된 숫자만큼 위에 있는 행을 선택합니다.
+            만약 이동하려는 행이 0보다 작다면 Error 100 을 출력합니다.
+            move -d <숫자>
+            현재 선택된 행에서 입력된 숫자만큼 아래에 있는 행을 선택합니다.
+            만약 이동하려는 행이 n 보다 크다면 Error 100 을 출력합니다.
+             */
+            if(commandArray[1]=="-u"){
+                if(currentSelect-commandArray[2].toInt()<0){
+                    println("Error 100")
+                    continue
+                }
+                currentSelect-=commandArray[2].toInt()
+            }
+            print("ok")
+        }
+        else if(commandArray[0]=="delete"){
+            print("ok")
+        }
+        else if(commandArray[0]=="restore"){
+            print("ok")
+        }
+        else if(commandArray[0]=="list"){
+            print("ok")
+        }
+        else if(commandArray[0]=="q") {
+            break
+        }
+    }
 }
