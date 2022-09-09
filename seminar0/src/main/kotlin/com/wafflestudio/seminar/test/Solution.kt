49491079acdc743c7c082511424bbad38b1a9413
jaylions <jaylions@snu.ac.kr>
2022-09-09 15:46:25 +0900

    delete done

--- seminar0/src/main/kotlin/com/wafflestudio/seminar/test/Solution.kt
+++ seminar0/src/main/kotlin/com/wafflestudio/seminar/test/Solution.kt
@@ -9,17 +9,17 @@ fun main() {
     // 여기를 채워 주세요!
     var firstInput=readLine();
     if (firstInput != null) {
-        val sliceNum=firstInput.length-2
+        val sliceNum=firstInput.length-3
         firstInput=firstInput.slice(IntRange(2,sliceNum))
     }
     println(firstInput)
-    var mainArray= firstInput?.split("\",\"")
+    val mainArray= firstInput?.split("\",\"")?.toMutableList()
     println(mainArray)
     var currentSelect=0;
     while (true){
-        var command= readLine().toString();
-        var commandArray=command.split(" ")
-        if(commandArray[0]=="move"){
+        val command= readLine().toString();
+        val commandArray=command.split(" ")
+        if(commandArray[0]=="move") {
             /*
             move -u <숫자>
             현재 선택된 행에서 입력된 숫자만큼 위에 있는 행을 선택합니다.
@@ -28,31 +28,32 @@ fun main() {
             현재 선택된 행에서 입력된 숫자만큼 아래에 있는 행을 선택합니다.
             만약 이동하려는 행이 n 보다 크다면 Error 100 을 출력합니다.
              */
-            if(commandArray[1]=="-u"){
-                if(currentSelect-commandArray[2].toInt()<0){
+            if (commandArray[1] == "-u") {
+                if (currentSelect - commandArray[2].toInt() < 0) {
                     println("Error 100")
                     continue
                 }
-                currentSelect-=commandArray[2].toInt()
-            }
-            else if(commandArray[1]=="-d"){
+                currentSelect -= commandArray[2].toInt()
+            } else if (commandArray[1] == "-d") {
                 if (mainArray != null) {
-                    if(currentSelect+commandArray[2].toInt()> mainArray.size-1){
+                    if (currentSelect + commandArray[2].toInt() > mainArray.size - 1) {
                         println("Error 100")
                         continue
                     }
-                currentSelect+=commandArray[2].toInt()
+                    currentSelect += commandArray[2].toInt()
                 }
-            
+                println(currentSelect)
+            }
         }
         else if(commandArray[0]=="delete"){
-            print("ok")
+                mainArray?.removeAt(currentSelect)
+                println(mainArray)
         }
         else if(commandArray[0]=="restore"){
             print("ok")
         }
         else if(commandArray[0]=="list"){
-            print("ok")
+            println(mainArray)
         }
         else if(commandArray[0]=="q") {
             break
