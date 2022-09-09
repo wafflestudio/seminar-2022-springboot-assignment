4ce4f90d22904a1f46a29f65043be287ca016b96
huGgW <whjoon0225@naver.com>
2022-09-09 23:58:25 +0900

    implemented interface of osrepository and surveyresponserepository

---
+++ seminar0/src/main/kotlin/com.wafflestudio.seminar/survey/database/OsRepositoryImpl.kt
@@ -0,0 +1,18 @@
+package com.wafflestudio.seminar.survey.database
+
+import com.wafflestudio.seminar.survey.domain.OperatingSystem
+import org.springframework.stereotype.Component
+
+@Component
+class OsRepositoryImpl(private val memoryDB: MemoryDB): OsRepository {
+    override fun findAll(): List<OperatingSystem> {
+        return memoryDB.getOperatingSystems()
+    }
+
+    override fun findById(id: Long): OperatingSystem {
+        val operatingSystems = findAll()
+        // TODO: Handle the case when not found (returning null)
+        return operatingSystems.find { it.id == id }!!
+    }
+
+}
---
+++ seminar0/src/main/kotlin/com.wafflestudio.seminar/survey/database/SurveyResponseRepositoryImpl.kt
@@ -0,0 +1,18 @@
+package com.wafflestudio.seminar.survey.database
+
+import com.wafflestudio.seminar.survey.domain.SurveyResponse
+import org.springframework.stereotype.Component
+
+@Component
+class SurveyResponseRepositoryImpl(private val memoryDB: MemoryDB): SurveyResponseRepository {
+
+    override fun findAll(): List<SurveyResponse> {
+        return memoryDB.getSurveyResponses()
+    }
+
+    override fun findById(id: Long): SurveyResponse {
+        val surveyResponses = findAll()
+        // TODO: Handle the case when not found (returning null)
+        return surveyResponses.find { it.id == id }!!
+    }
+}
