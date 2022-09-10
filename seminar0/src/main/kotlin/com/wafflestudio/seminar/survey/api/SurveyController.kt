c0e420b27b596a09e314b36b0de1d0b890f05c81
jaylions <jaylions@snu.ac.kr>
2022-09-10 21:32:44 +0900

    assignment 3 1,2 api done

--- seminar0/src/main/kotlin/com/wafflestudio/seminar/survey/api/SurveyController.kt
+++ seminar0/src/main/kotlin/com/wafflestudio/seminar/survey/api/SurveyController.kt
@@ -9,18 +9,18 @@ import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
-@RequestMapping("/api/v1/surveys")
+@RequestMapping("/api/v1/")
 class SurveyController(
     
     private val surveyService: SurveyService
 )
 {
-    @GetMapping("/")
+    @GetMapping("responses/")
     fun getAllResponses(): List<SurveyResponse> {
         return surveyService.getAllResponses()
     }
     
-    @GetMapping("/{id}/")
+    @GetMapping("responses/{id}/")
     fun geyResponseById(@PathVariable("id") id:Long):SurveyResponse{
         return surveyService.getResponseById(id)
     }
