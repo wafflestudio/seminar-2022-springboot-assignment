6d407f1ab32934af1f4c250eb004ef0df64585b1
jiwon79 <19-079@ksa.hs.kr>
2022-09-12 01:44:42 +0900

    :bug: Survey id가 없는 경우를 처리하기 위해 survey findById를 Optional로 변경

--- seminar0/src/main/kotlin/com/wafflestudio/seminar/survey/database/SurveyResponseRepository.kt
+++ seminar0/src/main/kotlin/com/wafflestudio/seminar/survey/database/SurveyResponseRepository.kt
@@ -1,8 +1,9 @@
 package com.wafflestudio.seminar.survey.database
 
 import com.wafflestudio.seminar.survey.domain.SurveyResponse
+import java.util.Optional
 
 interface SurveyResponseRepository {
     fun findAll(): List<SurveyResponse>
-    fun findById(id: Long): SurveyResponse
+    fun findById(id: Long): Optional<SurveyResponse>
 }
