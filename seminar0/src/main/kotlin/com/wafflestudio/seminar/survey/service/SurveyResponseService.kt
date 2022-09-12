e61460b76f958c53220cc5e185fc92ab4233e5ba
jiwon79 <19-079@ksa.hs.kr>
2022-09-13 01:43:00 +0900

    :sparkles: Survey Response Service 추가

---
+++ seminar0/src/main/kotlin/com/wafflestudio/seminar/survey/service/SurveyResponseService.kt
@@ -0,0 +1,24 @@
+package com.wafflestudio.seminar.survey.service
+
+import com.wafflestudio.seminar.survey.database.MemorySurveyResponseRepository
+import com.wafflestudio.seminar.survey.domain.SurveyResponse
+import org.springframework.stereotype.Service
+import java.util.Optional
+
+@Service
+class SurveyResponseService(
+    private val surveyResponseRepository: MemorySurveyResponseRepository
+) {
+    fun findSurveys(): List<SurveyResponse> {
+        return surveyResponseRepository.findAll()
+    }
+    
+    fun findById(id: Long): SurveyResponse {
+        val result: Optional<SurveyResponse> = surveyResponseRepository.findById(id)
+        if (result.isEmpty) {
+            throw IllegalArgumentException("SurveyResponse with id $id does not exist.")
+        }
+        
+        return result.get()
+    }
+}
