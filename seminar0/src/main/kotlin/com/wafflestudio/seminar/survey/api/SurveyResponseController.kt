d55b87a1567c60dead54ff208cc402bf1461ecd5
jiwon79 <19-079@ksa.hs.kr>
2022-09-14 21:59:31 +0900

    :goal_net: Survey Response catch error

--- seminar0/src/main/kotlin/com/wafflestudio/seminar/survey/api/SurveyResponseController.kt
+++ seminar0/src/main/kotlin/com/wafflestudio/seminar/survey/api/SurveyResponseController.kt
@@ -2,17 +2,30 @@ package com.wafflestudio.seminar.survey.api
 
 import com.wafflestudio.seminar.survey.domain.SurveyResponse
 import com.wafflestudio.seminar.survey.service.SurveyResponseService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RestControllerAdvice
 
-@RestController
+@RestControllerAdvice
 @RequestMapping("/survey")
 class SurveyResponseController(
     private val surveyResponseService: SurveyResponseService
 ) {
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handleIllegalArgumentException(e: IllegalArgumentException): ResponseEntity<String> {
+        return ResponseEntity(e.message ?: "Illegal Argument", HttpStatus.BAD_REQUEST)
+    }
+    
+    @ExceptionHandler(Exception::class)
+    fun handleException(e: Exception): ResponseEntity<String> {
+        return ResponseEntity(e.message ?: "Exception", HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+    
 
     @GetMapping("")
     fun getAll(): List<SurveyResponse> {
