4a4dd1e440e1ff78828d82aaa8a81a8f7b1697b1
jiwon79 <19-079@ksa.hs.kr>
2022-09-15 01:25:31 +0900

    :sparkles: Os Service 추가

---
+++ seminar0/src/main/kotlin/com/wafflestudio/seminar/survey/service/OsService.kt
@@ -0,0 +1,18 @@
+package com.wafflestudio.seminar.survey.service
+
+import com.wafflestudio.seminar.survey.database.OsRepositoryImpl
+import com.wafflestudio.seminar.survey.domain.OperatingSystem
+
+class OsService(
+    private val osRepository: OsRepositoryImpl
+) {
+    
+    fun findAllOs(): List<OperatingSystem> {
+        return osRepository.findAll()
+    }
+    
+    fun findOsById(id: Long): OperatingSystem {
+        val os: OperatingSystem? = osRepository.findById(id)
+        return os ?: throw IllegalArgumentException("No OS with id $id")
+    }
+}
