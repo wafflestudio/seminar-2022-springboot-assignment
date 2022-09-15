ee24faabfe06a977cae7578be68284391a3b51de
jiwon79 <19-079@ksa.hs.kr>
2022-09-15 14:51:40 +0900

    :sparkles: 3번 문제 Kotlin 으로 해결하기

--- seminar0/src/main/kotlin/com/wafflestudio/seminar/test/Solution.kt
+++ seminar0/src/main/kotlin/com/wafflestudio/seminar/test/Solution.kt
@@ -1,10 +1,72 @@
 package com.wafflestudio.seminar.test
 
-/**
- * TODO
- *   3번을 코틀린으로 다시 한번 풀어봐요.
- *   객체를 통한 구조화를 시도해보면 좋아요 :)
- */
+class Table(
+    private val table: MutableList<String>,
+    private var line: Int,
+): Cloneable {
+    public override fun clone(): Table {
+        return Table(table.toMutableList(), line)
+    }
+    fun getTarget(): String {
+        return table[line]
+    }
+    
+    fun setLineByTarget(target: String) {
+        line = table.indexOf(target)
+    }
+    
+    fun printTable() {
+        for (i in table) { println(i) }
+    }
+    
+    fun moveLine(option: String, moveLine: Int) {
+        when (option) {
+            "-u" -> {
+                if (line - moveLine >= 0) line -= moveLine
+                else println("Error 100")
+            }
+            "-d" -> {
+                if (line + moveLine < table.size) line += moveLine
+                else println("Error 100")
+            }
+        }
+    }
+    
+    fun deleteLine() {
+        table.removeAt(line)
+        if (line == table.size) line -= 1
+    }
+}
+
 fun main() {
-    // 여기를 채워 주세요!
+    val tableHistory: MutableList<Table> = mutableListOf()
+    
+    val input: String? = readLine()
+    val data: List<String> = input!!
+        .substring(1, input.length - 1)
+        .filter { it != '"' }
+        .split(",")
+    var table = Table(data.toMutableList(), 0)
+    
+    while (true) {
+        val command: String? = readLine()
+        if (command == "q") break
+        val commandSplit: List<String> = command!!.split(" ")
+        when (commandSplit[0]) {
+            "list" -> table.printTable()
+            "move" -> table.moveLine(commandSplit[1], commandSplit[2].toInt())
+            "delete" -> {
+                tableHistory.add(table.clone())
+                table.deleteLine()
+            }
+            "restore" -> {
+                if (tableHistory.size > 0) {
+                    val target: String = table.getTarget()
+                    table = tableHistory[tableHistory.size - 1]
+                    table.setLineByTarget(target)
+                    tableHistory.removeAt(tableHistory.size - 1)
+                } else println("Error 200")
+            }
+        } 
+    }
 }
