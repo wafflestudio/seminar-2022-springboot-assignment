ef4aa943e39cd37c78ad2abf55e241688d42a0c3
tasel220 <345t@naver.com>
2022-09-17 19:20:16 +0900

    recruit question 3

--- seminar0/src/main/kotlin/com/wafflestudio/seminar/test/Solution.kt
+++ seminar0/src/main/kotlin/com/wafflestudio/seminar/test/Solution.kt
@@ -5,6 +5,41 @@ package com.wafflestudio.seminar.test
  *   3번을 코틀린으로 다시 한번 풀어봐요.
  *   객체를 통한 구조화를 시도해보면 좋아요 :)
  */
+
+fun parseTipInteger(line : String) : Int
+{
+    val splitLine = line.split(' ');
+    return splitLine.last().toInt();
+}
 fun main() {
+    val firstLine = readLine();
+    val table = Table(firstLine);
+    
     // 여기를 채워 주세요!
+    while(true) {
+        val line = readLine() ?: ""
+        if (line == "q")
+            break;
+        else if (line == "list") {
+            table.printAll()
+        } else if (line == "delete") {
+            table.delete()
+        } else if (line == "restore") {
+            if(!table.restore())
+            {
+                println("Error 200")
+            }
+        } else if (line.contains("move")) {
+            val movement: Int = if (line.contains('u')) {
+                -parseTipInteger(line);
+            } else {
+                parseTipInteger(line);
+            }
+
+            if(!table.moveIndex(movement))
+            {
+                println("Error 100")
+            }
+        }
+    }
 }
