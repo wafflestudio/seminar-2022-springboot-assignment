8fa6146fc22d10f3ac66c374e30097bad4de4c49
woosuk.no <lkjs8269@naver.com>
2022-09-18 23:30:01 +0900

    Repository 구현

--- seminar0/src/main/kotlin/com/wafflestudio/seminar/survey/database/OsRepositoryImpl.kt
+++ seminar0/src/main/kotlin/com/wafflestudio/seminar/survey/database/OsRepositoryImpl.kt
@@ -1,19 +1,26 @@
 package com.wafflestudio.seminar.survey.database
 
-import com.wafflestudio.seminar.survey.api.SeminarExceptionHandler
+import com.wafflestudio.seminar.survey.exception.SeminarExceptionHandler
 import com.wafflestudio.seminar.survey.domain.OperatingSystem
+import com.wafflestudio.seminar.survey.exception.ErrorCode
+import com.wafflestudio.seminar.survey.exception.SeminarException
 import org.springframework.stereotype.Component
-import java.lang.Exception
 
 @Component
-class OsRepositoryImpl(private val memoryDB: MemoryDB, private val seminarExceptionHandler: SeminarExceptionHandler) :
+class OsRepositoryImpl(private val memoryDB: MemoryDB) :
     OsRepository {
     override fun findAll(): List<OperatingSystem> {
-        TODO("Not yet implemented")
+        return memoryDB.getOperatingSystems()
     }
 
     override fun findById(id: Long): OperatingSystem {
-        TODO("Not yet implemented")
+        val findResults = memoryDB.getOperatingSystems().find{it.id == id}
+        return findResults ?: throw SeminarException(ErrorCode.OS_NOT_FOUND)
+    }
+
+    override fun findByName(name: String): OperatingSystem {
+        val findResults = memoryDB.getOperatingSystems().find{it.osName == name}
+        return findResults ?: throw SeminarException(ErrorCode.OS_NOT_FOUND)
     }
 
 }
--- seminar0/src/main/kotlin/com/wafflestudio/seminar/survey/database/SurveyResponseRepositoryImpl.kt
+++ seminar0/src/main/kotlin/com/wafflestudio/seminar/survey/database/SurveyResponseRepositoryImpl.kt
@@ -1,17 +1,20 @@
 package com.wafflestudio.seminar.survey.database
 
-import com.wafflestudio.seminar.survey.api.SeminarExceptionHandler
+import com.wafflestudio.seminar.survey.exception.SeminarExceptionHandler
 import com.wafflestudio.seminar.survey.domain.SurveyResponse
+import com.wafflestudio.seminar.survey.exception.ErrorCode
+import com.wafflestudio.seminar.survey.exception.SeminarException
 import org.springframework.stereotype.Component
 
 @Component
-class SurveyResponseRepositoryImpl(private val memoryDB: MemoryDB, private val seminarExceptionHandler: SeminarExceptionHandler) :
+class SurveyResponseRepositoryImpl(private val memoryDB: MemoryDB) :
     SurveyResponseRepository {
     override fun findAll(): List<SurveyResponse> {
-        TODO("Not yet implemented")
+        return memoryDB.getSurveyResponses()
     }
 
     override fun findById(id: Long): SurveyResponse {
-        TODO("Not yet implemented")
+        val findResults = memoryDB.getSurveyResponses().find{it.id == id}
+        return findResults ?: throw SeminarException(ErrorCode.SURVEY_NOT_FOUND)
     }
 } 
