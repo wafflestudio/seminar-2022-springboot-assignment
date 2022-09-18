f2e8dc94113eaab07c23623be66c5b4a124794c0
Jack-Chagarr <netamijr@gmail.com>
2022-09-18 18:32:23 +0900

    commit2

--- seminar0/src/main/kotlin/com/wafflestudio/seminar/survey/api/SeminarExceptionHandler.kt
+++ seminar0/src/main/kotlin/com/wafflestudio/seminar/survey/api/SeminarExceptionHandler.kt
@@ -1,5 +1,8 @@
 package com.wafflestudio.seminar.survey.api
 
+import com.wafflestudio.seminar.survey.exception.idException
+import com.wafflestudio.seminar.survey.exception.osNameException
+import com.wafflestudio.seminar.survey.exception.osidException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -11,20 +14,32 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
  */
 @RestControllerAdvice
 class SeminarExceptionHandler {
-    @ExceptionHandler(value = [Exception::class])
+    /*@ExceptionHandler(value = [Exception::class])
     fun handle(e: Exception): ResponseEntity<Any> {
         return ResponseEntity("오류가 발생했어요!", HttpStatus.INTERNAL_SERVER_ERROR)
-    }
+    }*/
 
     /**
      * 상황에 맞는 적절한 예외를 미리 준비해둘 수도 있을 것 같아요.
      * 예외들은 어느 패키지에 있는게 적절할까요?
      * 예외는 어떤 정보를 공통적으로 담고 있을까요?
      */
-    @ExceptionHandler(value = [SeminarException::class])
-    fun handle(e: SeminarException): ResponseEntity<Any> {
-        TODO("적절한 ResponseBody & HttpStatus 조합을 내려줄 수도 있을 것 같다.")
+    @ExceptionHandler(value = [idException::class])
+    fun handle(e: idException): ResponseEntity<Any>{
+        return ResponseEntity("존재하지 않는 ID입니다.", HttpStatus.NOT_FOUND)
     }
 
-    inner class SeminarException() : RuntimeException()
+    @ExceptionHandler(value = [osidException::class])
+    fun handle(e: osidException): ResponseEntity<Any> {
+        return ResponseEntity("존재하지 않는 OS ID입니다.", HttpStatus.NOT_FOUND)
+    }
+
+    @ExceptionHandler(value = [osNameException::class])
+    fun handle(e: osNameException): ResponseEntity<Any> {
+        return ResponseEntity("존재하지 않는 OS NAME입니다.", HttpStatus.NOT_FOUND)
+    }
+
+    /*inner class idException(message: String) : IllegalArgumentException(message)
+    inner class osidException(message: String) : IllegalArgumentException(message)
+    inner class osNameException(message: String) : IllegalArgumentException(message)*/
 }
--- seminar0/src/main/kotlin/com/wafflestudio/seminar/test/Solution.kt
+++ seminar0/src/main/kotlin/com/wafflestudio/seminar/test/Solution.kt
@@ -1,10 +1,91 @@
 package com.wafflestudio.seminar.test
 
+import java.util.*
+import kotlin.collections.ArrayList
+
 /**
  * TODO
  *   3번을 코틀린으로 다시 한번 풀어봐요.
  *   객체를 통한 구조화를 시도해보면 좋아요 :)
  */
+class Solution(
+    private var stdlst : ArrayList<String>,) {
+     var history = Stack<ArrayList<String>>()
+    var cursor: String = stdlst[0]
+    fun move(dir: String, num: Int) {
+        if (dir == "u") {
+            if (stdlst.indexOf(cursor) - num < 0){
+                println("Error 100")
+                return
+            }else{
+                cursor = stdlst[stdlst.indexOf(cursor) - num]
+                return
+            }
+        }else{
+            if(stdlst.indexOf(cursor) + num >= stdlst.size){
+                println("Error 100")
+            }else{
+                cursor = stdlst[stdlst.indexOf(cursor) + num]
+                return
+            }
+        }
+    }
+    fun delete(){
+        val deletenum: Int = stdlst.indexOf(cursor)
+        if(deletenum == stdlst.size-1){
+            var copy = ArrayList<String>()
+            copy.addAll(stdlst)
+            history.push(copy)
+            cursor = stdlst[stdlst.indexOf(cursor)-1]
+            stdlst.remove(stdlst[deletenum])
+        }else{
+            var copy = ArrayList<String>()
+            copy.addAll(stdlst)
+            history.push(copy)
+            cursor = stdlst[stdlst.indexOf(cursor)+1]
+            stdlst.remove(stdlst[deletenum])
+        }
+    }
+    fun restore(){
+        if(history.isEmpty()){
+            println("Error 200")
+            return
+        }else{
+            stdlst = history.pop()
+        }
+    }
+    fun lst(): ArrayList<String>{
+        return stdlst
+    }
+}
 fun main() {
-    // 여기를 채워 주세요!
+    val sc: Scanner = Scanner(System.`in`)
+    var order = sc.nextLine()
+    order = order.substring(1,order.length-1)
+    val orderarrlst = order.split(",").toCollection(ArrayList<String>())
+    var solution = Solution(orderarrlst)
+    
+    while(true){
+        order = sc.nextLine()
+        val orderarr = order.split(" ")
+        if(orderarr[0] == "move"){
+            if(orderarr[1] == "-d"){
+                solution.move("d",  orderarr[2].toInt())
+            }else if(orderarr[1] == "-u"){
+                solution.move("u", orderarr[2].toInt())
+            }
+        }else if(orderarr[0] == "delete"){
+            solution.delete()
+        }else if(orderarr[0] == "restore"){
+            solution.restore()
+        }else if(orderarr[0] == "list"){
+            val students = solution.lst()
+            for(std in students){
+                println(std.substring(1,std.length-1))
+                
+            }
+        }else if(orderarr[0] == "q"){
+            break
+        }
+    }
 }
