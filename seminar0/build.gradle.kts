574d6b435bd4df9b2a35706af74f103944fe4f3d
Sumin-Kim-dev <sumin.kim.develop@gmail.com>
2022-09-19 16:02:45 +0900

    chore : 코드 수정시 자동으로 다시 빌드하도록 수정

--- seminar0/build.gradle.kts
+++ seminar0/build.gradle.kts
@@ -40,6 +40,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    implementation("org.springframework.boot:spring-boot-devtools")
 }
 
 tasks.withType<KotlinCompile> {
