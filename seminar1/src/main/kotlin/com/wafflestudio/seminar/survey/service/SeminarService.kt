cf02f655fd344357037579cb9d5e18fed291d3ee
Boseong Seo <90292371+Boseong-Seo@users.noreply.github.com>
2022-09-30 02:20:52 +0900

    설문조사 참여 API 생성

--- seminar1/src/main/kotlin/com/wafflestudio/seminar/survey/service/SeminarService.kt
+++ seminar1/src/main/kotlin/com/wafflestudio/seminar/survey/service/SeminarService.kt
@@ -1,34 +1,43 @@
 package com.wafflestudio.seminar.survey.service
 
-import com.wafflestudio.seminar.survey.api.Seminar404
+import com.wafflestudio.seminar.exception.ErrorCode
+import com.wafflestudio.seminar.exception.SeminarException
+import com.wafflestudio.seminar.survey.api.request.CreateSurveyRequest
 import com.wafflestudio.seminar.survey.database.OperatingSystemEntity
 import com.wafflestudio.seminar.survey.database.OsRepository
 import com.wafflestudio.seminar.survey.database.SurveyResponseEntity
 import com.wafflestudio.seminar.survey.database.SurveyResponseRepository
 import com.wafflestudio.seminar.survey.domain.OperatingSystem
 import com.wafflestudio.seminar.survey.domain.SurveyResponse
+import com.wafflestudio.seminar.user.database.UserRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 interface SeminarService {
     fun os(name: String): OperatingSystem
     fun os(id: Long): OperatingSystem
     fun surveyResponseList(): List<SurveyResponse>
     fun surveyResponse(id: Long): SurveyResponse
+    fun postSurvey(userId: Long, req: CreateSurveyRequest): String
 }
 
 @Service
 class SeminarServiceImpl(
     private val surveyResponseRepository: SurveyResponseRepository,
     private val osRepository: OsRepository,
+    private val userRepository: UserRepository
 ) : SeminarService {
+    
     override fun os(id: Long): OperatingSystem {
-        val entity = osRepository.findByIdOrNull(id) ?: throw Seminar404("OS를 찾을 수 없어요.")
+        val entity = osRepository.findByIdOrNull(id)
+            ?: throw SeminarException(ErrorCode.OS_NOT_FOUND)
         return OperatingSystem(entity)
     }
 
     override fun os(name: String): OperatingSystem {
-        val entity = osRepository.findByOsName(name) ?: throw Seminar404("OS ${name}을 찾을 수 없어요.")
+        val entity = osRepository.findByOsName(name)
+            ?: throw SeminarException(ErrorCode.OS_NOT_FOUND)
         return OperatingSystem(entity)
     }
 
@@ -38,10 +47,41 @@ class SeminarServiceImpl(
     }
 
     override fun surveyResponse(id: Long): SurveyResponse {
-        val surveyEntity = surveyResponseRepository.findByIdOrNull(id) ?: throw Seminar404("설문 결과를 찾을 수 없어요.")
+        val surveyEntity = surveyResponseRepository.findByIdOrNull(id)
+            ?: throw SeminarException(ErrorCode.SURVEY_NOT_FOUND)
         return SurveyResponse(surveyEntity)
     }
 
+
+    override fun postSurvey(userId: Long, req: CreateSurveyRequest): String {
+        // 유저 정보 찾기
+        val userEntity = userRepository.findByIdOrNull(userId)
+            ?: throw SeminarException(ErrorCode.USER_NOT_FOUND)
+        
+        // OS 정보 찾기
+        val osEntity = osRepository.findByOsName(req.os!!)
+            ?: throw SeminarException(ErrorCode.OS_NOT_FOUND)
+        
+        surveyResponseRepository.save(
+            SurveyResponseEntity(
+                user = userEntity,
+                operatingSystem = osEntity,
+                springExp = req.springExp!!,
+                rdbExp = req.rdbExp!!,
+                programmingExp = req.programmingExp!!,
+                major = req.major,
+                grade = req.grade,
+                timestamp = LocalDateTime.now(),
+                backendReason = req.backendReason,
+                waffleReason = req.waffleReason,
+                somethingToSay = req.somethingToSay
+            )
+        )
+        
+        return "설문에 참여해주셔서 감사합니다."
+    }
+    
+    
     private fun OperatingSystem(entity: OperatingSystemEntity) = entity.run {
         OperatingSystem(id, osName, price, desc)
     }
@@ -49,6 +89,7 @@ class SeminarServiceImpl(
     private fun SurveyResponse(entity: SurveyResponseEntity) = entity.run {
         SurveyResponse(
             id = id,
+            user = user?.toUser(),
             operatingSystem = OperatingSystem(operatingSystem),
             springExp = springExp,
             rdbExp = rdbExp,
@@ -58,7 +99,7 @@ class SeminarServiceImpl(
             timestamp = timestamp,
             backendReason = backendReason,
             waffleReason = waffleReason,
-            somethingToSay = somethingToSay,
+            somethingToSay = somethingToSay
         )
     }
 }
