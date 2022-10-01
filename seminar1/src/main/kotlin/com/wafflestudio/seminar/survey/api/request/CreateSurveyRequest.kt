56b3df6f5b54072e679f86094a686a1a86d644be
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-10-02 08:16:25 +0900

    feat : POST /api/v1/survey 를 위한 Request

--- seminar1/src/main/kotlin/com/wafflestudio/seminar/survey/api/request/CreateSurveyRequest.kt
+++ seminar1/src/main/kotlin/com/wafflestudio/seminar/survey/api/request/CreateSurveyRequest.kt
@@ -1,6 +1,29 @@
 package com.wafflestudio.seminar.survey.api.request
 
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotNull
+
 data class CreateSurveyRequest(
-    val major: String,
-    // TODO 채워서, 이용해주세요!
+    val major: String? = null,
+    
+    @field:NotNull(message = "os 이름을 입력하세요")
+    val os: String,
+
+    @field:Min(1, message = "springExp는 1 이상 5 이하의 정수입니다")
+    @field:Max(5, message = "springExp는 1 이상 5 이하의 정수입니다")
+    val springExp: Int,
+
+    @field:Min(1, message = "rdbExp는 1 이상 5 이하의 정수입니다")
+    @field:Max(5, message = "rdbExp는 1 이상 5 이하의 정수입니다")
+    val rdbExp: Int,
+
+    @field:Min(1, message = "programmingExp는 1 이상 5 이하의 정수입니다")
+    @field:Max(5, message = "programmingExp는 1 이상 5 이하의 정수입니다")
+    val programmingExp: Int,
+    
+    val grade: String? = null,
+    val backendReason: String? = null,
+    val waffleReason: String? = null,
+    val somethingToSay: String? = null,
 )
