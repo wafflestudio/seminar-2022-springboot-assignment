0ebd7497ec725463132a8e3dda63018b738df9b9
huGgW <whjoon0225@naver.com>
2022-10-02 00:34:39 +0900

    Add POST /api/v1/survey for adding survey, needed to be fixed request body error handling

--- seminar1/src/main/kotlin/com/wafflestudio/seminar/survey/api/request/CreateSurveyRequest.kt
+++ seminar1/src/main/kotlin/com/wafflestudio/seminar/survey/api/request/CreateSurveyRequest.kt
@@ -1,6 +1,16 @@
 package com.wafflestudio.seminar.survey.api.request
 
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+
 data class CreateSurveyRequest(
-    val major: String,
-    // TODO 채워서, 이용해주세요!
+        @param: NotBlank val os: String,
+        @param: NotNull val spring_exp: Int,
+        @param: NotNull val rdb_exp: Int,
+        @param: NotNull val programming_exp: Int,
+        val major: String = "",
+        val grade: String = "",
+        val backendReason: String? = null,
+        val waffleReason: String? = null,
+        val somethingToSay: String? = null
 )
