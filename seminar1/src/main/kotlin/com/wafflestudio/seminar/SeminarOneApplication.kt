8ace5821b677f701cd044fccb5260b940f632955
jiwon79 <19-079@ksa.hs.kr>
2022-10-03 20:29:11 +0900

    :sparkles: h2-console 연결

--- seminar1/src/main/kotlin/com/wafflestudio/seminar/SeminarOneApplication.kt
+++ seminar1/src/main/kotlin/com/wafflestudio/seminar/SeminarOneApplication.kt
@@ -1,9 +1,11 @@
 package com.wafflestudio.seminar
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableJpaAuditing
 class SeminarOneApplication
 
 fun main(args: Array<String>) {
