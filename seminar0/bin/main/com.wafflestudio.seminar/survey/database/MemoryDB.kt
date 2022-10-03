1115a7ed665614316b875976cfb93fa2052e9c64
woosuk.no <lkjs8269@naver.com>
2022-10-03 18:24:12 +0900

    Init Seminar1 Assignment

---
+++ seminar0/bin/main/com.wafflestudio.seminar/survey/database/MemoryDB.kt
@@ -0,0 +1,63 @@
+package com.wafflestudio.seminar.survey.database
+
+import com.wafflestudio.seminar.survey.domain.OperatingSystem
+import com.wafflestudio.seminar.survey.domain.SurveyResponse
+import org.springframework.boot.context.event.ApplicationStartedEvent
+import org.springframework.context.event.EventListener
+import org.springframework.core.io.ClassPathResource
+import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+@Component
+class MemoryDB {
+    private val operatingSystems = mutableListOf<OperatingSystem>()
+    private val surveyResponses = mutableListOf<SurveyResponse>()
+
+    fun getOperatingSystems(): List<OperatingSystem> {
+        return operatingSystems
+    }
+
+    fun getSurveyResponses(): List<SurveyResponse> {
+        return surveyResponses
+    }
+
+    /**
+     * 서버가 시작하면, 엑셀 파일을 메모리로 불러오는 역할을 수행해요
+     * 지금 당장 이해할 필요는 없어요!
+     */
+    @EventListener
+    fun loadExcel(event: ApplicationStartedEvent) {
+        loadOS()
+        loadSurveyResponses()
+    }
+
+    private fun loadOS() = operatingSystems.addAll(
+        listOf(
+            OperatingSystem(1L, "MacOS", 300000L, "Most favorite OS of Seminar Instructors"),
+            OperatingSystem(2L, "Linux", 0L, "Linus Benedict Torvalds"),
+            OperatingSystem(3L, "Windows", 0L, "Window.."),
+        )
+    )
+
+    private fun loadSurveyResponses() {
+        val responses = ClassPathResource("data/example_surveyresult.tsv")
+            .file
+            .readLines()
+            .mapIndexed { idx, it ->
+                val rawSurveyResponse = it.split("\t")
+                SurveyResponse(
+                    id = idx.toLong(),
+                    timestamp = LocalDateTime.parse(rawSurveyResponse[0], DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                    operatingSystem = operatingSystems.find { os -> os.osName == rawSurveyResponse[1] }!!,
+                    springExp = rawSurveyResponse[2].toInt(),
+                    rdbExp = rawSurveyResponse[3].toInt(),
+                    programmingExp = rawSurveyResponse[4].toInt(),
+                    major = rawSurveyResponse[5],
+                    grade = rawSurveyResponse[6],
+                )
+            }
+
+        surveyResponses.addAll(responses)
+    }
+}
---
+++ seminar0/bin/main/com.wafflestudio.seminar/survey/domain/SurveyResponse.kt
@@ -0,0 +1,17 @@
+package com.wafflestudio.seminar.survey.domain
+
+import java.time.LocalDateTime
+
+data class SurveyResponse(
+    val id: Long,
+    val operatingSystem: OperatingSystem,
+    val springExp: Int,
+    val rdbExp: Int,
+    val programmingExp: Int,
+    val major: String,
+    val grade: String,
+    val timestamp: LocalDateTime,
+    val backendReason: String? = null,
+    val waffleReason: String? = null,
+    val somethingToSay: String? = null
+)
