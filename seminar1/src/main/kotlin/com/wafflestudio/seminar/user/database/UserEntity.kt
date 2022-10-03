931e9651565520a808793882e5957109fd33935b
jiwon79 <19-079@ksa.hs.kr>
2022-10-03 21:39:54 +0900

    :sparkles: User Entity 추가

---
+++ seminar1/src/main/kotlin/com/wafflestudio/seminar/user/database/UserEntity.kt
@@ -0,0 +1,15 @@
+package com.wafflestudio.seminar.user.database
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "user")
+class UserEntity(
+    val name: String,
+    val email: String,
+    val password: String,
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0L
+}
