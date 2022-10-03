ca1287386e6fed1ebb7233d2b9cf6675509c75fa
강지혁 <wlgur7238@snu.ac.kr>
2022-10-04 04:45:03 +0900

    과제 2 명세 및 스켈레톤 업데이트

---
+++ seminar2/src/main/kotlin/com/wafflestudio/seminar/SeminarAssignmentTwo.kt
@@ -0,0 +1,11 @@
+package com.wafflestudio.seminar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SeminarOneApplication
+
+fun main(args: Array<String>) {
+    runApplication<SeminarOneApplication>(*args)
+}
