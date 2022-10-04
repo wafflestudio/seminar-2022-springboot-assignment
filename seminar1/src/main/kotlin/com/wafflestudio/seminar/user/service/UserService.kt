70c5e302cfc9693e7347058672c7728b39c45969
jiwon79 <19-079@ksa.hs.kr>
2022-10-04 11:31:48 +0900

    :recycle: UserService interface 에서 Entity 가 아니라 User 를 리턴하도록 변경

--- seminar1/src/main/kotlin/com/wafflestudio/seminar/user/service/UserService.kt
+++ seminar1/src/main/kotlin/com/wafflestudio/seminar/user/service/UserService.kt
@@ -1,10 +1,11 @@
 package com.wafflestudio.seminar.user.service
 
+import com.wafflestudio.seminar.user.api.request.CreateUserRequest
 import com.wafflestudio.seminar.user.database.UserEntity
-import java.util.*
+import com.wafflestudio.seminar.user.domain.User
 
 interface UserService {
     fun getAll(): List<UserEntity>
-    fun getUserById(id: Long): Optional<UserEntity>
-    fun createUser(user: UserEntity): UserEntity
+    fun getUserById(id: Long): User
+    fun createUser(user: CreateUserRequest): User
 }
