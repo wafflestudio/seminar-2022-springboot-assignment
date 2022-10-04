966a717f9d091f0fc2da885d9123ce8ef58ca0a6
jiwon79 <19-079@ksa.hs.kr>
2022-10-04 14:08:05 +0900

    :sparkles: User Exception handler 추가

---
+++ seminar1/src/main/kotlin/com/wafflestudio/seminar/user/api/UserExceptionHandler.kt
@@ -0,0 +1,20 @@
+package com.wafflestudio.seminar.user.api
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class UserExceptionHandler {
+    @ExceptionHandler(value = [Exception::class])
+    fun handle(e: Exception): ResponseEntity<Any> {
+        return ResponseEntity("오류가 발생했어요!", HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    @ExceptionHandler(value = [UserException::class])
+    fun handle(e: UserException
+    ): ResponseEntity<Any> {
+        return ResponseEntity(e.message, e.status)
+    }
+}
