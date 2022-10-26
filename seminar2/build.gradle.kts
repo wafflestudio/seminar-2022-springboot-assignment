fc6249dcef2fdf0b6a043157250b5a634130bdb2
huGgW <whjoon0225@naver.com>
2022-10-26 12:41:49 +0900

    added module 'javax.validation:validation-api'

--- seminar2/build.gradle.kts
+++ seminar2/build.gradle.kts
@@ -38,6 +38,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("mysql:mysql-connector-java")
+    
+    implementation("javax.validation:validation-api")
 
 
     // Auth
