a3af640843517d1d5dba56942cebf7ab8625a6f3
DESKTOP-USQPRVG\gram <blueland1998@naver.com>
2022-10-28 10:51:30 +0900

    getSeminarById 추가(1)

--- seminar2/src/main/kotlin/com/wafflestudio/seminar/core/user/dto/CreateSeminarInstructorDto.kt
+++ seminar2/src/main/kotlin/com/wafflestudio/seminar/core/user/dto/CreateSeminarInstructorDto.kt
@@ -1,10 +1,10 @@
 package com.wafflestudio.seminar.core.user.dto
 
+import java.time.LocalDateTime
+
 data class CreateSeminarInstructorDto(
     val id: Long?,
     val username: String?,
     val email: String?,
-    val joinedAt: String?
-) {
-
-}
+    val joinedAt: LocalDateTime?
+)
