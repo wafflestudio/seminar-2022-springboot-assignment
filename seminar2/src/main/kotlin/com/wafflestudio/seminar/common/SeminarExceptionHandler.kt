2266aabc123ace4d75941faa41c6680f969e011e
leeeryboy <indiv0227@snu.ac.kr>
2022-10-30 21:18:52 +0900

    FEAT: Config ExceptionHandlers

--- seminar2/src/main/kotlin/com/wafflestudio/seminar/common/SeminarExceptionHandler.kt
+++ seminar2/src/main/kotlin/com/wafflestudio/seminar/common/SeminarExceptionHandler.kt
@@ -1,10 +1,28 @@
 package com.wafflestudio.seminar.common
 
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.security.SignatureException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class SeminarExceptionHandler {
-    
-    // TODO as-you-wish
-    
+
+    @ExceptionHandler(value = [SeminarException::class])
+    fun handle(e: SeminarException): ResponseEntity<Any> {
+        return ResponseEntity(e.message, e.status)
+    }
+
+    @ExceptionHandler(value = [SignatureException::class])
+    fun handle(e: SignatureException): ResponseEntity<Any> {
+        return ResponseEntity("User Authentication Failed", HttpStatus.FORBIDDEN)
+    }
+
+    @ExceptionHandler(value = [ExpiredJwtException::class])
+    fun handle(e: ExpiredJwtException): ResponseEntity<Any> {
+        return ResponseEntity(e.message, HttpStatus.FORBIDDEN)
+    }
+
 }
