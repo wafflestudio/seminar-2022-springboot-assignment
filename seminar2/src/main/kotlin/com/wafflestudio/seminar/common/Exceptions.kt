3e9741aa87bbe958cc5e050e350a1e123c0df269
dongjoocha <dongjoocha@naver.com>
2022-11-01 06:21:20 +0900

    Implement exception handling

--- seminar2/src/main/kotlin/com/wafflestudio/seminar/common/Exceptions.kt
+++ seminar2/src/main/kotlin/com/wafflestudio/seminar/common/Exceptions.kt
@@ -7,3 +7,11 @@ open class SeminarException(msg: String, val status: HttpStatus) : RuntimeExcept
 class Seminar404(msg: String) : SeminarException(msg, HttpStatus.NOT_FOUND)
 
 class Seminar400(msg: String) : SeminarException(msg, HttpStatus.BAD_REQUEST)
+
+class Seminar409(msg: String) : SeminarException(msg, HttpStatus.CONFLICT)
+
+class Seminar401(msg: String) : SeminarException(msg, HttpStatus.UNAUTHORIZED)
+
+class Seminar403(msg: String) : SeminarException(msg, HttpStatus.FORBIDDEN)
+
+class Seminar200(msg: String) : SeminarException(msg, HttpStatus.OK)
