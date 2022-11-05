28b72150413c00e35c7a2a48aad11542ba6c1e24
Boseong Seo <90292371+Boseong-Seo@users.noreply.github.com>
2022-11-05 13:02:35 +0900

    프로필 수정 요청 API 수정 - request body 수정   + role 필드 삭제시 함께 삭제해야 했던 유효성 내용 삭제 누락

--- seminar2/src/main/kotlin/com/wafflestudio/seminar/core/user/api/request/EditRequest.kt
+++ seminar2/src/main/kotlin/com/wafflestudio/seminar/core/user/api/request/EditRequest.kt
@@ -7,7 +7,6 @@ data class EditRequest(
     val email: String? = null,
     val username: String? = null,
     val password: String? = null,
-    @field:Pattern(regexp= "INSTRUCTOR|PARTICIPANT", message="유형 값을 올바르게 입력해주세요.")
     val company: String? = null,
     @field:Positive(message = "0 이상의 값을 입력해주세요.")
     val year: Int? = null,
