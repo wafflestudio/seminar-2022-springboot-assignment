07b78b4d3325161d4bd0913982a4e5c32b2b0cb3
DESKTOP-USQPRVG\gram <blueland1998@naver.com>
2022-11-06 14:42:29 +0900

    SeminarServiceTest - createSeminar 생성

--- seminar2/src/test/kotlin/com/wafflestudio/seminar/core/seminar/SeminarServiceTest.kt
+++ seminar2/src/test/kotlin/com/wafflestudio/seminar/core/seminar/SeminarServiceTest.kt
@@ -1,4 +1,39 @@
 package com.wafflestudio.seminar.core.seminar
 
-class SeminarServiceTest {
+import com.wafflestudio.seminar.core.seminar.api.request.CreateSeminarRequest
+import com.wafflestudio.seminar.core.seminar.database.SeminarEntity
+import com.wafflestudio.seminar.core.seminar.database.SeminarRepository
+import com.wafflestudio.seminar.core.seminar.database.UserSeminarEntity
+import com.wafflestudio.seminar.core.seminar.service.SeminarService
+import com.wafflestudio.seminar.core.user.UserTestHelper
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.stereotype.Component
+
+@SpringBootTest
+internal class SeminarServiceTest @Autowired constructor(
+        private val seminarService: SeminarService,
+        private val seminarRepository: SeminarRepository,
+        private val userTestHelper: UserTestHelper
+){
+    
+    @Test
+    fun `세미나를 등록할 수 있다`(){
+        
+        // given
+        val user = userTestHelper.createInstructorUser("w1@ffle.com", password = "1234")
+        val seminar = CreateSeminarRequest("seminar1",30,16,"12:30")
+        
+        // when
+        val result = seminarService.createSeminar(user.id,seminar)
+        
+        // then
+        assertThat(seminarRepository.findAll()).hasSize(1)
+        assertThat(result.name).isEqualTo("seminar1")
+    }
+
+
+  
 }
