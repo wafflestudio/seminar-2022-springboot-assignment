5059352a4e508d4e76d5d75e12399f70df448e22
DESKTOP-USQPRVG\gram <blueland1998@naver.com>
2022-11-08 15:18:40 +0900

    SeminarServcieTest - getSeminar, searchSeminar 추가

--- seminar2/src/test/kotlin/com/wafflestudio/seminar/core/seminar/SeminarServiceTest.kt
+++ seminar2/src/test/kotlin/com/wafflestudio/seminar/core/seminar/SeminarServiceTest.kt
@@ -58,6 +58,63 @@ internal class SeminarServiceTest @Autowired constructor(
         assertThat(result.name).isEqualTo("seminar1")
     }
 
+    @Test
+    @Transactional
+    fun `Id로 세미나를 조회할 수 있다`() {
+
+        // when
+        for (n: Int in 1..10) {
+            createSeminars(n)
+        }
+
+        // given
+        val result = seminarService.getSeminar(3)
+
+        // then
+        assertThat(result.id).isEqualTo(3)
+
+    }
+    @Test
+    @Transactional
+    fun `원하는 순서로 세미나 전체를 조회할 수 있다`() {
+
+        // when
+        for (n: Int in 1..10) {
+            createSeminars(n)
+        }
+
+        // given
+        val result1 = seminarService.searchSeminar(null, null)
+        val result2 = seminarService.searchSeminar(null,"earliest")
+
+
+        // then
+        assertThat(result1).hasSize(10)
+        assertThat(result2).hasSize(10)
+        assertThat(result1[1].name).isEqualTo("seminar9")
+        assertThat(result2[1].name).isEqualTo("seminar2")
+    }
+
+
+
+    @Test
+    @Transactional
+    fun `이름으로 세미나를 조회할 수 있다`() {
+
+        // when
+        for (n: Int in 1..10) {
+            createSeminars(n)
+        }
+
+        // given
+        val result = seminarService.searchSeminar("seminar2",null)
+
+        // then
+        assertThat(result).hasSize(1)
+        assertThat(result[0].name).isEqualTo("seminar2")
+
+    }
+
 
 
     @Test
@@ -83,7 +140,7 @@ internal class SeminarServiceTest @Autowired constructor(
     private fun createSeminars(n: Int) {
 
         val user = userTestHelper.createInstructorUser("w$n@ffle.com", password = "1234")
-        val seminar = CreateSeminarRequest("seminar1", 30, 16, "12:30")
+        val seminar = CreateSeminarRequest("seminar$n", 30, 16, "12:30")
         seminarService.createSeminar(user.id, seminar)
 
     }
