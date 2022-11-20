9ffb40bab4884737c01c5b11226c112a48146d6f
DESKTOP-USQPRVG\gram <blueland1998@naver.com>
2022-11-20 19:51:28 +0900

    login 코드 정리

--- seminar2/src/main/kotlin/com/wafflestudio/seminar/core/user/service/AuthService.kt
+++ seminar2/src/main/kotlin/com/wafflestudio/seminar/core/user/service/AuthService.kt
@@ -2,6 +2,7 @@ package com.wafflestudio.seminar.core.user.service;
 
 import com.wafflestudio.seminar.common.Seminar400
 import com.wafflestudio.seminar.common.Seminar401
+import com.wafflestudio.seminar.common.Seminar404
 import com.wafflestudio.seminar.core.user.api.request.LoginRequest
 import com.wafflestudio.seminar.core.user.api.request.SignUpRequest
 import com.wafflestudio.seminar.core.user.database.*
@@ -11,6 +12,7 @@ import com.wafflestudio.seminar.core.user.domain.UserEntity
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service;
 import java.time.LocalDate
+import javax.transaction.Transactional
 
 
 @Service
@@ -24,6 +26,7 @@ class AuthService(
         if (userRepository.findByEmail(request.email) != null) {
             throw Seminar400("해당 이메일은 이미 가입되어 있습니다")
         }
+        
         val encodedPassword = this.passwordEncoder.encode(request.password)
 
         if(request.instructor != null){
@@ -31,7 +34,9 @@ class AuthService(
                 throw Seminar400("연도에는 0 또는 양의 정수만 입력할 수 있습니다")
             }
         }
+        
         val userEntity = UserEntity.of(request, encodedPassword)
+        
         if(request.role == "PARTICIPANT" && request.participant != null){
             val participantProfileEntity = ParticipantProfileEntity.of(userEntity,request.participant)
             userEntity.participant = participantProfileEntity
@@ -45,29 +50,23 @@ class AuthService(
         return authTokenService.generateTokenByEmail(request.email)
     }
 
+    @Transactional
     fun login(request: LoginRequest): AuthToken {
 
-        val userEntity = userRepository.findByEmail(request.email)
-        if (this.passwordEncoder.matches(request.password, userEntity?.password)) {
+        val userEntity = userRepository.findByEmail(request.email) ?: throw Seminar404("해당 이메일로 가입된 유저가 없습니다")
+        
+        if (this.passwordEncoder.matches(request.password, userEntity.password)) {
             val token = authTokenService.generateTokenByEmail(request.email)
             val lastLogin = LocalDate.from(authTokenService.getCurrentIssuedAt(token.accessToken))
-            loginEntity(request.email, lastLogin)
+            userEntity.lastLogin = lastLogin 
             return token
         } else {
-            throw Seminar401("인증이 되지 않았습니다")
+            throw Seminar401("해당 비밀번호로 인증이 되지 않았습니다")
         }
 
     }
 
     
-
-    private fun loginEntity(email: String, lastLogin: LocalDate) {
-        val userEntity = userRepository.findByEmail(email)
-        userEntity?.lastLogin = lastLogin
-        if (userEntity != null) {
-            userRepository.save(userEntity)
-        }
-    }
 
 
 
