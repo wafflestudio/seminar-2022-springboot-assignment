82419e211d3fa879d11ebfd6fd0b4e71fe771f35
woong97 <yjwoong97@gmail.com>
2022-11-26 01:36:56 +0900

    add rds sql configuration

--- seminar2/build.gradle.kts
+++ seminar2/build.gradle.kts
@@ -62,6 +62,11 @@ dependencies {
     implementation("com.querydsl:querydsl-core:$querydslVersion")
     kapt("com.querydsl:querydsl-apt:$querydslVersion:jpa")
     kapt(group = "com.querydsl", name = "querydsl-apt", classifier = "jpa")
+    
+    // Swagger
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.0")
+    implementation("org.springdoc:springdoc-openapi-kotlin:1.6.0")
+    
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
