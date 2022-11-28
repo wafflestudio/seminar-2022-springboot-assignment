64d53bf93dbd606d1b3328c8188f6b8403d06ff5
Chanhyuk Yang <yangchanhk98@snu.ac.kr>
2022-11-28 17:09:09 +0900

    build.grale.kts 변경

--- seminar2/build.gradle.kts
+++ seminar2/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("org.springframework.boot") version "2.7.2"
     id("io.spring.dependency-management") version "1.0.12.RELEASE"
     kotlin("jvm") version "1.6.21"
-    kotlin("kapt") version "1.7.10"         /////////////////////
+    kotlin("kapt") version "1.7.10"
     kotlin("plugin.spring") version "1.6.21"
     kotlin("plugin.jpa") version "1.6.21"
     kotlin("plugin.allopen") version "1.3.71"
@@ -37,8 +37,6 @@ dependencies {
     // Web & DB
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("mysql:mysql-connector-java")
 
@@ -65,8 +63,15 @@ dependencies {
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
+    // Validation
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    
+    // AOP
     implementation("org.springframework.boot:spring-boot-starter-aop")
+    
+    // Swagger
+    implementation("org.springdoc:springdoc-openapi-ui")
+    implementation("org.springdoc:springdoc-openapi-kotlin")
 }
 
 // QueryDSL
