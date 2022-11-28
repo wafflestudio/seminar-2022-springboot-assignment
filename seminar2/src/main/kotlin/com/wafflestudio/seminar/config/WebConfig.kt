5e07828505deeec23a63e03cb3b909a36bb95f4a
곽민지 <beinte0419@snu.ac.kr>
2022-11-28 20:46:35 +0900

    [chore] 사용하지 않는 import 지움

--- seminar2/src/main/kotlin/com/wafflestudio/seminar/config/WebConfig.kt
+++ seminar2/src/main/kotlin/com/wafflestudio/seminar/config/WebConfig.kt
@@ -1,7 +1,6 @@
 package com.wafflestudio.seminar.config
 
 import com.wafflestudio.seminar.core.user.service.AuthInterceptor
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
